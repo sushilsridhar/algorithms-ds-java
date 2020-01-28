4abdcbb195f63771b17abb195641e44117d19551
Sushil Sridhar <sushilsridhar16@gmail.com>
2020-01-28 07:19:42 +0530

    Completed insertion sort

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -3,26 +3,27 @@ package sorting;
 public class InsertionSort {
     public static void main(String[] args) {
 
+        //int arr[] = { 67,55 ,24,34,53, 45,31,34};
         int arr[] = {12, 11, 13, 5, 6};
 
         int length = arr.length;
         int marker = 1;
-        int unsortedArrayLength = 1;
+        int sortedArrayLength = 1;
 
         while(marker < length) {
+            int indexToBeCompared = marker;
 
-            for(int i = unsortedArrayLength -1; i >= 0; i--) {
-                System.out.println("------marker="+marker+" i="+i);
-                if(arr[marker] < arr[i]) {
-                    int temp = arr[marker];
-                    arr[marker] = arr[i];
+            for(int i = sortedArrayLength -1; i >= 0; i--) {
+                if(arr[indexToBeCompared] < arr[i]) {
+                    int temp = arr[indexToBeCompared];
+                    arr[indexToBeCompared] = arr[i];
                     arr[i] = temp;
+                    indexToBeCompared = i;
                 }
-                Utility.printArray(arr);
             }
             marker++;
-            unsortedArrayLength++;
+            sortedArrayLength++;
         }
-
+        Utility.printArray(arr);
     }
 }
