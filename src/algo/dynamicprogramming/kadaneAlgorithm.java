c6117bb08afe607d62dc105fe014bc2e7ff98fa7
Sushil <sushilsridhar16@gmail.com>
2022-02-01 11:34:22 +0530

    kadane algorithm

--- src/algo/dynamicprogramming/kadaneAlgorithm.java
+++ src/algo/dynamicprogramming/kadaneAlgorithm.java
@@ -1,28 +1,55 @@
 package algo.dynamicprogramming;
 
+/*
+    Technique: kadane's algorithm
+
+    Find the contiguous subarray within an array, A of length N which has the largest sum.
+
+    5, 6, 7, -3, 2, -10, -12, 8, 12, 21 --> ans = 41
+
+    The subarray [8 12 21] has the maximum possible sum of 41
+
+    A = [-2,1,-3,4,-1, 2, 1, -5, 4] --> ans = 6, subarray [4, -1, 2, 1]
+    A=  [-2, -3, -4, -1, -2, -1, -5, -4] --> ans = -1, subarray [-1]
+
+    approach:
+
+      _ 7 13  2 4 _
+     -5
+
+     7 13 2 4 is a subarray with sum 26,
+     suppose first blank, that is element before 7 is -5, we should not consider the subarray [-5 7 13  2 4], sum 21, which decreases the total sum
+
+     _  _ 7 13  2 4 _
+    12 -5
+
+    now 12 + (-5) -> 7, 7 is positive so this number will contribute to the total sum, subarray [12 -5 7 13  2 4], sum = 33, increases the total sum
+
+    if previous subarray sum is positive, consider it as subarray,
+    if previous subarray sum is negative, ignore the subarray, and consider the subarray starting from current index
+ */
 public class kadaneAlgorithm {
 
     public static void main(String[] args) {
 
-        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int[] nums = { 5, 6, 7, -3, 2, -10, -12, 8, 12, 21 }; // output -> 41
 
-        maxSubArray(nums);
+        System.out.println(kadaneAlgo(nums));
     }
 
-    public static int maxSubArray(int[] nums) {
-        // Initialize our variables using the first element.
-        int currentSubarray = nums[0];
-        int maxSubarray = nums[0];
+    public static int kadaneAlgo(int[] a) {
 
-        // Start with the 2nd element since we already used the first one.
-        for (int i = 1; i < nums.length; i++) {
-            int num = nums[i];
-            // If current_subarray is negative, throw it away. Otherwise, keep adding to it.
-            currentSubarray = Math.max(num, currentSubarray + num);
-            maxSubarray = Math.max(maxSubarray, currentSubarray);
+        int currentSum = 0, max = Integer.MIN_VALUE;
+        for(int i=0; i<a.length; i++) {
+
+            currentSum = currentSum + a[i];
+            max = Math.max(max, currentSum);
+
+            if(currentSum < 0) {
+                currentSum = 0;
+            }
         }
-
-        return maxSubarray;
+        return max;
     }
 
 }
